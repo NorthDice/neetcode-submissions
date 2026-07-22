1422d023bb58cbc3b664d55aac4819a9492fe8be
Balychev <149325976+NorthDice@users.noreply.github.com>
2026-07-22 19:12:12 +0100

    Add: binary-search - submission-3

---
+++ Data Structures & Algorithms/binary-search/submission-3.go
@@ -0,0 +1,20 @@
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1 
+	}
+	l,r := 0,len(nums)-1
+	
+	for l <= r {
+		mid := l + (r-l)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
+}
