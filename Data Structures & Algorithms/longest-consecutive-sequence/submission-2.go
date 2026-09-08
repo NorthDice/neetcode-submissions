1d0069a926e663407377c2b7ba8b96c18bd65769
Balychev <149325976+NorthDice@users.noreply.github.com>
2026-09-08 21:52:44 +0100

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.go
@@ -0,0 +1,30 @@
+// 2,20,4,10,3,4,5 
+func longestConsecutive(nums []int) int {
+	ints := make(map[int]struct{}, len(nums))
+	for _, num := range nums {
+		ints[num] = struct{}{}
+	}
+
+	
+	maxSeq := 0
+
+	for v := range ints {
+		if _, ok := ints[v-1];ok {
+			continue
+		} 
+		currentNum := v
+        currentSeq := 1
+
+		for {
+			if _, ok := ints[currentNum+1]; ok {
+				currentSeq++
+				currentNum++
+			} else {
+				break
+			}
+		}
+		maxSeq = max(maxSeq, currentSeq)
+		
+	}
+	return maxSeq
+}
