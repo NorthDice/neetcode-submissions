a14d7026fc571e2b89159b5acc10054ba79094cc
Balychev <149325976+NorthDice@users.noreply.github.com>
2026-09-18 17:59:38 +0100

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.go
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p != nil && q != nil && p.Val == q.Val {
+		return isSameTree(q.Left, p.Left) && isSameTree(q.Right, p.Right)
+	}
+
+	return false
+}
